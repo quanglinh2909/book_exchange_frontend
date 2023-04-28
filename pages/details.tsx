2c45733c0f1eca2144352029da362b2739529bcd
chanhhiep <19130067@st.hcmuaf.edu.vn>
2023-04-28 15:38:52 +0700

    category admin

--- pages/details.tsx
+++ pages/details.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, MenuItem, Typography } from "@mui/material";
 import { useState } from "react";
 
 export interface IUploadsProps {
@@ -9,9 +9,31 @@ export default function Details(props: IUploadsProps) {
     setImage("https://avatars.githubusercontent.com/u/91894488?s=60&v=4");
     return (
         <Box >
-            <Box sx={{backgroundColor:'white'}}>
-                <Box sx={{ width: '200px', height: '200px' ,display:'none'}} component={'img'} src={image}>
+            <Box sx={{ backgroundColor: 'white' }}>
+                <Box sx={{ width: '200px', height: '200px', display: 'none' }} component={'img'} src={image}>
                 </Box>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={6}>
+                        <MenuItem>
+                        <Box sx={{ width: '20px', height: '20px' ,display:'none'}} component={'img'} src={image}></Box>
+                        </MenuItem>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <MenuItem>
+                        <Box sx={{ width: '20px', height: '20px' ,display:'none'}} component={'img'} src={image}></Box>
+                        </MenuItem>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <MenuItem>
+                        <Box sx={{ width: '20px', height: '20px' ,display:'none'}} component={'img'} src={image}></Box>
+                        </MenuItem>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <MenuItem>
+                        <Box sx={{ width: '20px', height: '20px' ,display:'none'}} component={'img'} src={image}></Box>
+                        </MenuItem>
+                    </Grid>
+                </Grid>
             </Box>
             <Typography variant='h1' sx={{ fontSize: '32px', marginBottom: '10px' }}>Uploads</Typography>
         </Box>
