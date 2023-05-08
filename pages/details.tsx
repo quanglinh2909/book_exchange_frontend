2da0f4c045e80a88f228f2e39c214bf1d3b47d54
chanhhiep <19130067@st.hcmuaf.edu.vn>
2023-05-08 12:48:10 +0700

    fix cate

--- pages/details.tsx
+++ pages/details.tsx
@@ -5,11 +5,9 @@ export interface IUploadsProps {
 }
 export default function Details(props: IUploadsProps) {
     const [image, setImage] = useState('');
-    //test image
-    setImage("https://avatars.githubusercontent.com/u/91894488?s=60&v=4");
     return (
-        <Box >
-            <Box sx={{ backgroundColor: 'white' }}>
+        <Box sx={{ backgroundColor: 'white',display:'flex',flexDirection:'row' }}>
+            <Box sx={{ }}>
                 <Box sx={{ width: '200px', height: '200px', display: 'none' }} component={'img'} src={image}>
                 </Box>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
@@ -35,7 +33,10 @@ export default function Details(props: IUploadsProps) {
                     </Grid>
                 </Grid>
             </Box>
-            <Typography variant='h1' sx={{ fontSize: '32px', marginBottom: '10px' }}>Uploads</Typography>
+            <Box>
+               <Typography variant='h1' sx={{ fontSize: '32px', marginBottom: '10px' }}>Name Product</Typography>
+               
+            </Box>
         </Box>
     );
 }
