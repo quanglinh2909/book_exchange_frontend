8c48e326519536c129ea26895c16a9b9ff8d2a60
DucHung8102001 <19130082@st.hcmuaf.edu.vn>
2023-05-17 23:19:02 +0700

    no message

--- pages/notification.tsx
+++ pages/notification.tsx
@@ -1,9 +1,15 @@
 import Main2Layout from "@/components/common/layout/main2";
+import { Stack } from "@mui/material";
+import { Typography } from "antd";
 
 export interface INotificationProps {}
 
 function Notification(props: INotificationProps) {
-  return <div></div>;
+  
+  return 
+  <Stack>
+    <Typography>Notifications</Typography>
+  </Stack>
 }
 Notification.Layout = Main2Layout;
 export default Notification;
